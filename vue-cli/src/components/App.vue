ccaf44500ee89b69b0cc2dbede76ee3b72fee96c
Jonathan Zarate <zaratedev@gmail.com>
2017-12-14 18:23:18 -0600

    Installation and use of Vue Router in Vue.js

--- vue-cli/src/components/App.vue
+++ vue-cli/src/components/App.vue
@@ -1,8 +1,14 @@
 <template>
   <div id="app" class="container">
     <h1>Curso de Vue - zaratedev</h1>
+    <router-link to="/">Dashboard</router-link>
+    <router-link to="/tasks">Tareas</router-link>
+
+    <keep-alive>
+      <router-view></router-view>
+    </keep-alive>
     
-    <app-tasks></app-tasks>
+
     <footer class="footer">
       <p>&copy; 2017 zaratedev.com</p>
     </footer>
@@ -10,11 +16,8 @@
 </template>
 
 <script>
-  import Tasks from "components/Task/Task.vue"
   export default {
-    components: {
-      'app-tasks': Tasks
-    }
+
   }
 </script>
 
--- vue-cli/src/main.js
+++ vue-cli/src/main.js
@@ -1,8 +1,29 @@
 import Vue from 'vue'
+import Router from 'vue-router'
 import EventBus from 'event-bus.js'
 import App from './components/App.vue'
+import Dashboard from './components/Dashboard.vue'
+import Tasks from './components/Task/Task.vue'
 
-new Vue({
+Vue.use(Router)
+
+var router = new Router({
+	routes: [
+		{
+			path: '/',
+			component: Dashboard
+		},
+		{
+			path: '/tasks',
+			component: Tasks
+		}
+	]
+});
+
+window.EventBus = EventBus
+
+var vm = new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
